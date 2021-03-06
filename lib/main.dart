3904a8d71f70f48f2d4aed4ce1927d35ac9910eb
David Kalil Braga <dmmd10@hotmail.com>
2021-03-05 22:19:53 -0300

    Cadastro e login de usuario no firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:olx/Home.dart';
+import 'file:///C:/Users/david/Desktop/Flutter/olx/lib/views/Home.dart';
 
 final ThemeData temaPadrao = ThemeData(
   primaryColor: Color(0xff9c27b0),
   accentColor: Color(0xff7b1fa2)
 );
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     title: "OLX",
     home: Home(),
