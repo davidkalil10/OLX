46fd74f45f821988b4dc54a1a1b74b217ae47fb6
David Kalil Braga <dmmd10@hotmail.com>
2021-03-06 20:18:13 -0300

    Adicionando dropdown menu

--- lib/views/NovoAnuncio.dart
+++ lib/views/NovoAnuncio.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
+import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:olx/views/widgets/BotaoCustomizado.dart';
+import 'package:validadores/Validador.dart';
 
 class NovoAnuncio extends StatefulWidget {
   @override
@@ -12,8 +14,12 @@ class NovoAnuncio extends StatefulWidget {
 class _NovoAnuncioState extends State<NovoAnuncio> {
 
   List<PickedFile> _listaImagens = [];
-
+  List<DropdownMenuItem<String>> _listaItensDropEstados = [];
+  List<DropdownMenuItem<String>> _listaItensDropCategorias = [];
+  String _itemSelecionadoEstado;
+  String _itemSelecionadoCategoria;
   final _formKey = GlobalKey<FormState>();
+
 
   _selecionarImagemGalera() async{
 
@@ -26,6 +32,31 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
     }
 
   }
+
+  _carregarItensDropDown(){
+    //Estados
+    for(var estado in Estados.listaEstadosSigla){
+      _listaItensDropEstados.add(DropdownMenuItem(child: Text(estado),value: estado));
+      print("Aqui: " +estado);
+    }
+
+    //Categorias
+    _listaItensDropCategorias.add(DropdownMenuItem(child: Text("Automóvel"),value: "auto"));
+    _listaItensDropCategorias.add(DropdownMenuItem(child: Text("Imóvel"),value: "imovel"));
+    _listaItensDropCategorias.add(DropdownMenuItem(child: Text("Eletrônicos"),value: "eletro"));
+    _listaItensDropCategorias.add(DropdownMenuItem(child: Text("Moda"),value: "moda"));
+    _listaItensDropCategorias.add(DropdownMenuItem(child: Text("Esportes"),value: "esportes"));
+
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarItensDropDown();
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -148,8 +179,56 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
                 //menu dropdown
                 Row(
                   children: [
-                    Text("Estado"),
-                    Text("Categoria"),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: DropdownButtonFormField(
+                          hint: Text("Estados"),
+                          value: _itemSelecionadoEstado,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20
+                          ),
+                          items: _listaItensDropEstados,
+                          validator: (valor){
+                            return Validador()
+                                .add(Validar.OBRIGATORIO,msg: "Campo obrigatório")
+                                .valido(valor);
+                          },
+                          onChanged: (valor){
+                            setState(() {
+                              _itemSelecionadoEstado = valor;
+                              print(valor);
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: DropdownButtonFormField(
+                          hint: Text("Categorias"),
+                          value: _itemSelecionadoCategoria,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20
+                          ),
+                          items: _listaItensDropCategorias,
+                          validator: (valor){
+                            return Validador()
+                                .add(Validar.OBRIGATORIO,msg: "Campo obrigatório")
+                                .valido(valor);
+                          },
+                          onChanged: (valor){
+                            setState(() {
+                              _itemSelecionadoCategoria = valor;
+                              print(valor);
+                            });
+                          },
+                        ),
+                      ),
+                    ),
                   ],
                 ),
                 //caixa de texto e botoes
